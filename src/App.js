f9041dced60f96667e6bb580b9bd56886a163c81
Boulay.M <macboulay2@gmail.com>
2025-03-31 17:11:24 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { Route, Routes, Link } from "react-router-dom";
 import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import Realisations from "./pages/Realisations";
@@ -9,15 +11,23 @@ import MentionsLegales from "./pages/MentionsLegales";
 
 export default function App () {
     return (
+        <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark" fixed="top" className="nav">
+            <Container>
+          <Navbar.Brand>John Doe</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto" variant="underline">
+                    <Link to="/" className="nav-link">Accueil</Link>
+                    <Link to="/services" className="nav-link">Services</Link>
+                    <Link to="/contact" className="nav-link">Me contacter</Link>
+                    <Link to="/realisations" className="nav-link">Realisations</Link>
+                    <Link to="/mentionslegales" className="nav-link">Mentions Légales</Link>
+            </Nav>
+          </Navbar.Collapse>
+          </Container>
+      </Navbar>
 
-        <div className="App">
-                <nav className="App-nav">
-                    <Link to="/">Accueil</Link>
-                    <Link to="/services">Services</Link>
-                    <Link to="/contact">Me contacter</Link>
-                    <Link to="/realisations">Realisations</Link>
-                    <Link to="/mentionslegales">Mentions Légales</Link>
-                </nav>
             <Routes>
                 <Route path="/" element = {<Accueil/>}></Route>
                 <Route path="services" element = {<Services/>}></Route>
