6dea7957e2b5891d11da62a9b9b24baa7d6d69eb
Boulay.M <macboulay2@gmail.com>
2025-04-01 10:59:17 +0200

    add home-1

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,17 +1,15 @@
 import "../App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Image, Container } from "react-bootstrap";
 
 const Accueil = ()=>{
     return (
-        <div className="App-body">
-        <h1>Ma page APP</h1>
-        <h2>Accueil</h2>
-        <button type="button" className="btn btn-primary">Primary</button>
-        <div>
-        <i className="bi bi-alarm" style={{ fontSize: '2rem', color: 'red' }}></i>
-        </div>
+        <div className="hero ratio ratio-1x1">
+            <div className="hero-txt fs-1 fw-bolder">
+                <h1>Bonjour, je suis John Doe</h1>
+                <h2>Développeur web full stack</h2>
+            </div>
         </div> 
-
     )
 }
 export default Accueil;
